dc8414fa2f5bb799b79939e39c6760760a5695c5
Daniel <daniel@dev.la>
2021-08-15 15:26:44 +0200

    use all attributes from svgo string

--- src/svg-vue.vue
+++ src/svg-vue.vue
@@ -1,7 +1,6 @@
 <template>
     <svg
-        :viewBox="svgViewBoxValues"
-        xmlns="http://www.w3.org/2000/svg"
+        v-bind="svgAttributes"
         v-html="svgContent"
     >
     </svg>
@@ -20,10 +19,22 @@ export default {
     setup(props) {
         const iconPath = computed(() => props.icon.replace(new RegExp('.'.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1'), 'g'), '/') + '.svg');
         const svgString = computed(() => require(`svg-files-path/${iconPath.value}`).default);
-        const svgViewBoxValues = computed(() => svgString ? (/viewBox="([^"]+)"/.exec(svgString.value) || '')[1] : null);
+        const svgAttributes = computed(() => {
+            if (! svgString) return {};
+
+            let wrapper = document.createElement('div');
+            wrapper.innerHTML = svgString.value;
+
+            let attributesList = wrapper.firstElementChild.attributes;
+            let attributes = {};
+
+            Object.keys(attributesList).map(i => attributes[attributesList[i].name] = attributesList[i].value);
+
+            return attributes;
+        });
         const svgContent = computed(() => svgString ? svgString.value.replace(/^<svg[^>]*>|<\/svg>$/g, '') : null);
 
-        return { svgViewBoxValues, svgContent };
-    },
+        return { svgAttributes, svgContent };
+    }
 }
 </script>
